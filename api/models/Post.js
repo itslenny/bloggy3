5d0a19fa283251e9612bebd261198a75cf13ee0d
Lenny <lenny@itslennysfault.com>
2015-09-01 12:49:45 -0700

    basic api

---
+++ api/models/Post.js
@@ -0,0 +1,35 @@
+/**
+* Post.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    title:{
+      type:'string',
+      required: true
+    },
+    body:{
+      type:'text',
+      required:true
+    },
+    comments:{
+      collection:'Comment',
+      via:'post'
+    },
+
+    tags:{
+      collection:'Tag',
+      via:'posts'
+    }
+  }
+};
+
+
+
+
+
+
